fe4b608661b4d40e84fe8011bd8bc6a46310abc5
Hanson Wong <Dismounted@users.noreply.github.com>
2016-07-31 03:00:31 +1000

    Add dashboard mockup.

--- web/resources/views/dashboard.blade.php
+++ web/resources/views/dashboard.blade.php
@@ -3,9 +3,42 @@
 @section('title', 'Dashboard')
 
 @section('content')
-    <main class="row">
-        <div class="col-sm-12">
-            <h1>Hello world!</h1>
+    <main class="row dashboard">
+        <div class="col-sm-3">
+            <div class="text-center">
+                <img src="{{ asset('images/fall.png') }}">
+            </div>
+            <div class="alert alert-success">
+                <h4>Test</h4>
+                <p>Testing!</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="text-center">
+                <img src="{{ asset('images/pill.png') }}">
+            </div>
+            <div class="alert alert-warning">
+                <h4>Test</h4>
+                <p>Testing!</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="text-center">
+                <img src="{{ asset('images/door.png') }}">
+            </div>
+            <div class="alert alert-danger">
+                <h4>Test</h4>
+                <p>Testing!</p>
+            </div>
+        </div>
+        <div class="col-sm-3">
+            <div class="text-center">
+                <img src="{{ asset('images/stove.png') }}">
+            </div>
+            <div class="alert alert-info">
+                <h4>Test</h4>
+                <p>Testing!</p>
+            </div>
         </div>
     </main>
 @endsection
